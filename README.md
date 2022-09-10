c8cf91f2f95f604c027b93803ed984751301e9d4
Davis Banks <davis.e.banks@gmail.com>
2022-09-10 15:48:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learning Go
+
+This is a side project to push myself to learn go and do another side 
+project.
---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+document.documentElement.dataset.scroll = "0";
+
+document.addEventListener('scroll', () => {
+    document.documentElement.dataset.scroll = "" + window.scrollY;
+});
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
